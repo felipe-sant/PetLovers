1cda2dc9e320723f50b4e3ac424dc334f35728ea
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-04-16 07:03:44 -0300

    mudando o nome das paginas

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,8 +2,8 @@ import { Route, BrowserRouter, Routes as Switch } from "react-router-dom";
 import NotFound from "../pages/notFound.page";
 import React from "react";
 import Home from "../pages/home.page";
-import Clientes from "../pages/clientes";
-import Produtos from "../pages/produtos";
+import Clientes from "../pages/clientes.page";
+import Produtos from "../pages/produtos.page";
 import Servicos from "../pages/servicos.page";
 
 class Routes extends React.Component {
