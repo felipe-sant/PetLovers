8244572b35a61166d5ac22f130a9bab50d3e07f0
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-04-16 12:01:03 -0300

    adicionando props no cliente

--- src/components/itemCliente.component.tsx
+++ src/components/itemCliente.component.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import styles from "../styles/itemCliente.module.css"
 
-class ItemCliente extends React.Component {
+type ItemClienteProps = {
+    nome:string,
+    nomeSocial:string,
+    dataRegistro:string,
+    cpf:string,
+    email:string,
+    rg:string[],
+    telefones:string[],
+    pets:string[],
+    produtos:string[],
+    servicos:string[]
+}
+
+class ItemCliente extends React.Component<ItemClienteProps> {
     render() {
+        const { nome, nomeSocial, dataRegistro, cpf, email, rg, telefones, pets, produtos, servicos } = this.props;
+
         return (
             <div className={styles.item}>
                 <div className={styles.cabecalho}>
                     <div className={styles.titulo}>
-                        <h1>nome_social</h1>
-                        <h2>nome_cliente</h2>
+                        <h1>{nome}</h1>
+                        <h2>{nomeSocial}</h2>
                     </div>
                     <div className={styles.detalhes}>
                         <span className={styles.dataRegistro}>
-                            00/00/0000
+                            {dataRegistro}
                         </span>
                     </div>
                 </div>
@@ -21,11 +36,11 @@ class ItemCliente extends React.Component {
                     <div className={styles.informacoesSimples}>
                         <div className={styles.informacao}>
                             <h3>CPF:</h3>
-                            <p>000.000.000.00</p>
+                            <p>{cpf}</p>
                         </div>
                         <div className={styles.informacao}>
                             <h3>Email:</h3>
-                            <p>email_do_cliente@gmail.com</p>
+                            <p>{email}</p>
                         </div>
                         <div className={styles.pets}>
                             <button>
@@ -37,15 +52,17 @@ class ItemCliente extends React.Component {
                         <div className={styles.informacao}>
                             <h3>RGs:</h3>
                             <ul>
-                                <li>rg1</li>
-                                <li>rg2</li>
+                                {rg.map((rg, index) => {
+                                    return <li key={index}>{rg}</li>
+                                })}
                             </ul>
                         </div>
                         <div className={styles.informacao}>
                             <h3>Telefones:</h3>
                             <ul>
-                                <li>rg1</li>
-                                <li>rg2</li>
+                                {telefones.map((telefone, index) => {
+                                    return <li key={index}>{telefone}</li>
+                                })}
                             </ul>
                         </div>
                     </div>
--- src/pages/clientes.page.tsx
+++ src/pages/clientes.page.tsx
@@ -26,8 +26,30 @@ class Clientes extends React.Component {
                                 <BotaoAdicionar texto="Adicionar" effect={AdicionarCliente} />
                             </div>
                             <div className={styles.listagem}>
-                                <ItemCliente />
-                                <ItemCliente />
+                                <ItemCliente 
+                                    nome="Gustavo"
+                                    nomeSocial=""
+                                    dataRegistro="13/09/2021"
+                                    cpf="000.000.000-00"
+                                    email="emailexemplo@gmail.com"
+                                    rg={["00.000.000-0", "00.000.000-0"]}
+                                    telefones={["(00) 00000-0000", "(00) 00000-0000"]}
+                                    pets={["Pet1", "Pet2"]}
+                                    produtos={["Produto1", "Produto2"]}
+                                    servicos={["Servico1", "Servico2"]}
+                                />
+                                <ItemCliente 
+                                    nome="Pedro Augusto"
+                                    nomeSocial="Akira dos Santos"
+                                    dataRegistro="13/09/2021"
+                                    cpf="000.000.000-00"
+                                    email="emailexemplo@gmail.com"
+                                    rg={["00.000.000-0", "00.000.000-0"]}
+                                    telefones={["(00) 00000-0000", "(00) 00000-0000"]}
+                                    pets={["Pet1", "Pet2"]}
+                                    produtos={["Produto1", "Produto2"]}
+                                    servicos={["Servico1", "Servico2"]}
+                                />
                             </div>
                         </main>
                     </div>
