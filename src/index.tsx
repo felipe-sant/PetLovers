b2663155e22e270db27cdd1d6e2d853aba53a783
Luiz Felipe <felipe.pinghang2@gmail.com>
2024-04-18 15:29:32 -0300

    adicionando arquivos base

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Roteador from './componentes/roteador';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
   <React.StrictMode>
-    <Roteador />
+    <App />
   </React.StrictMode>
 );
 
